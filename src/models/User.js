7406c0e12d7599b148bb55b1f804942c4c6f696c
Zebra <zebrv.apps@gmail.com>
2022-04-23 22:02:36 +0200

    remove placeholder func

--- src/models/User.js
+++ src/models/User.js
@@ -9,10 +9,6 @@ class User {
   static from(json) {
     return Object.assign(new User(), json);
   }
-
-  static placeholderImage() {
-    return "https://www.business2community.com/wp-content/uploads/2017/08/blank-profile-picture-973460_640.png";
-  }
 }
 
 export default User;
