df339a77a0f1391833888b163a1cc3a127164395
Zebra <zebrv.apps@gmail.com>
2022-04-23 15:13:41 +0200

    script de checkeo

--- check.sh
+++ check.sh
@@ -1,5 +1,23 @@
 #!/usr/bin/env bash
 
-npm run build --if-present
-npm run format
-npm run lint
+function check {
+  if [ $? -eq 0 ]; then
+    echo "✅ $1"
+  else
+    echo "❌ $1: Ejecuta $2 para más información"
+  fi
+}
+
+npm run build --if-present >/dev/null 2>&1
+check "Compilación" "npm run build"
+
+npm run test --script-shell >/dev/null 2>&1
+check "Tests" "npm run test"
+
+# Ejecutar el formateador y despues checkear, siempre debería estar bien
+npm run format >/dev/null 2>&1
+npm run check-format >/dev/null 2>&1
+check "Format" "npm run check-format"
+
+npm run lint >/dev/null 2>&1
+check "Linter" "npm run lint"
