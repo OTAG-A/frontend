0b07f110ffbb784005aae98cfb0eec5616bbaea4
Zebra <zebrv.apps@gmail.com>
2022-04-24 16:54:06 +0200

    user preview method

--- src/environment/UserProvider.js
+++ src/environment/UserProvider.js
@@ -5,18 +5,8 @@ import { User } from "../models";
 // UserContext stores the current logged user or None if not logged.
 export var UserContext = React.createContext();
 
-const user = User.from({
-  name: "Test user",
-  email: "test@test.com",
-  creationDate: Date.now(),
-  messagesNum: 500,
-  image:
-    "http://upload.wikimedia.org/wikipedia/commons/thumb/3/3a/Cat03.jpg/220px-Cat03.jpg",
-  isAdmin: true,
-});
-
 function UserProvider({ children }) {
-  return <UserContext.Provider value={user}>{children}</UserContext.Provider>;
+  return <UserContext.Provider value={User.preview()}>{children}</UserContext.Provider>;
 }
 
 export default UserProvider;
--- src/models/User.js
+++ src/models/User.js
@@ -9,6 +9,18 @@ class User {
   static from(json) {
     return Object.assign(new User(), json);
   }
+
+  static preview() {
+    return User.from({
+      name: "Test user",
+      email: "test@test.com",
+      creationDate: Date.now(),
+      messagesNum: 500,
+      image:
+        "http://upload.wikimedia.org/wikipedia/commons/thumb/3/3a/Cat03.jpg/220px-Cat03.jpg",
+      isAdmin: true,
+    });
+  }
 }
 
 export default User;
