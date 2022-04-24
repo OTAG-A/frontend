c3b407bbbb80dde5c7215d4c051fbe194934d916
Zebra <zebrv.apps@gmail.com>
2022-04-24 22:43:35 +0200

    temporary animal model

---
+++ src/models/Animal.js
@@ -0,0 +1,69 @@
+// TODO: Modelo temporal, sujeto a cambios.
+class Animal {
+  id = 0;
+  ficha = "";
+  raza = "";
+  sexo = "";
+  fechaNac = Date();
+  edad = "";
+  tamagno = "";
+  peso = "";
+  observaciones = "";
+  nombre = "";
+  especie = "";
+  color = "";
+  chenil = "";
+  peligroso = false;
+  microchip = "";
+  cartilla = "";
+  rabia = false;
+  fechaRabia = Date();
+  esterilizado = false;
+  perdido = false;
+  evaluacion = "";
+  caracter = "";
+  observacionesVet = "";
+  fechaIngreso = Date();
+  formaEntrada = "";
+  entrada = "";
+  urgencias = false;
+  horaUrgencias = "";
+  numPolicia = "";
+  nombreSolicitante = "";
+  apellidosSolicitante = "";
+  dniSolicitante = "";
+  telefonoSolicitante = "";
+  domicilioSolicitante = "";
+  mailSolicitante = "";
+  disponible = "";
+  fechaAdopcion = Date();
+  nombreInteresado = "";
+  apellidosInteresado = "";
+  dniInteresado = "";
+  telefonoInteresado = "";
+  mailInteresado = "";
+  direccionInteresado = "";
+  tasa = "";
+  importe = 0;
+  pago = "";
+  bloquear = false;
+  desbloquear = false;
+  fechaBloqueo = Date();
+
+  static from(json) {
+    return Object.assign(new Animal(), json);
+  }
+
+  static preview() {
+    return Animal.from({
+      id: 0,
+      sexo: "masculino",
+      fechaNac: Date.now(),
+      tamagno: "chiquito",
+      nombre: "Misifú",
+      imagen: "http://upload.wikimedia.org/wikipedia/commons/thumb/3/3a/Cat03.jpg/220px-Cat03.jpg",
+    });
+  }
+}
+
+export default Animal;
