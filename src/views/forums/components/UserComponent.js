e8a397d6717d4be65f457876905d8cd0d8dad7d0
Zebra <zebrv.apps@gmail.com>
2022-04-27 12:23:03 +0200

    post user component

---
+++ src/views/forums/components/UserComponent.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+
+// UserComponent describes the image with the username under it.
+function UserComponent({ user }) {
+  const navigate = useNavigate();
+
+  return (
+    <div
+      className="col-3 user text-center d-flex flex-column"
+      onClick={() => navigate("/usuario/" + user.id)}
+    >
+      <div className="">
+        <img
+          src={user.image}
+          className="rounded-circle"
+          style={{
+            backgroundColor: "lightgray",
+          }}
+          width={60}
+          alt={user.name} />
+      </div>
+      <div className="">
+        <b>{user.name}</b>
+      </div>
+    </div>
+  );
+}
+
+export default UserComponent;
