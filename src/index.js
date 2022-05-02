af1d96a0039ab3e5180f1b053cc9eb44f60772aa
Leticia-23 <760549@unizar.es>
2022-05-02 15:11:55 +0200

    footer provisional

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import reportWebVitals from "./reportWebVitals";
+import Footer from "./views/Footer";
 
 import {
   Home,
@@ -37,7 +38,7 @@ ReactDOM.render(
           <Route path="/about" element={<About />} />
         </Routes>
 
-        {/* <Footer /> */}
+        <Footer />
       </Router>
     </UserProvider>
   </React.StrictMode>,
