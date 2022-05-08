e50ae0985cfae6552806b3320353f00be9a25b1d
Leticia-23 <760549@unizar.es>
2022-05-08 21:15:22 +0200

    Detalles animal

--- src/views/animals/AnimalDetails.js
+++ src/views/animals/AnimalDetails.js
@@ -1,29 +1,62 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useContext } from "react";
 import { useParams } from "react-router";
+import { Animal } from "../../models";
+import ComunDetails from "./components/ComunDetails";
+import { UserContext } from "../../environment";
 
 function AnimalDetails() {
   let { idAnimal } = useParams();
+  let currentUser = useContext(UserContext);
 
   useEffect(() => {
     // TODO: Obtener detalles de animal de la API dado su ID
   }, [idAnimal]);
 
+  const animal = Animal.preview();
+
   return (
-    <div className="home">
-      <header className="mt-5 p-5">
-        <h1 className="font-weight-light text-center fw-bold">
-          Animal {idAnimal}
-        </h1>
-      </header>
-      <div className="container">
-        <div className="row align-items-center">
-          <p>
-            Lorem Ipsum is simply dummy text of the printing and typesetting
-            industry. Lorem Ipsum has been the industry's standard dummy text
-            ever since the 1500s, when an unknown printer took a galley of type
-            and scrambled it to make a type specimen book.
-          </p>
-        </div>
+    <div className="animalDetails">
+      <ComunDetails animal={animal} />
+      <div className="row my-3">
+        {currentUser ? (
+          <div className="row">
+            <h3>Detalles de adopción</h3>
+            <div className="row mx-5">
+              <ul>
+                <li class="my-5">
+                  <b> Descripción: </b> {animal.observaciones}
+                </li>
+                <li class="my-5">
+                  <b> Observaciones: </b> {animal.observacionesVet}
+                </li>
+                <li class="my-5">
+                  <b> Solicitante: </b> {animal.nombreSolicitante}
+                </li>
+                <li class="my-5">
+                  <b> Dirección del solicitante: </b>{" "}
+                  {animal.domicilioSolicitante}
+                </li>
+              </ul>
+            </div>
+          </div>
+        ) : (
+          <div className="row align-items-center px-5">
+            <div className="container px-5">
+              <p
+                className="text-center m-5"
+                style={{ backgroundColor: "orange", color: "black" }}
+              >
+                Para más información y detalles de adopción es necesario{" "}
+                <b>
+                  estar{" "}
+                  <a href={"/registro"} style={{ color: "black" }}>
+                    registrado
+                  </a>
+                </b>
+              </p>
+            </div>
+          </div>
+        )}
       </div>
     </div>
   );
