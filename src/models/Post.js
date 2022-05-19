8d6afbe03c3d3887f64c99a5a66214ea0a829102
Zebra <zebrv.apps@gmail.com>
2022-05-19 15:14:50 +0200

    css changes and post model sync

--- src/models/Post.js
+++ src/models/Post.js
@@ -2,11 +2,10 @@ import { default as User } from "./User";
 import { default as Comment } from "./Comment";
 
 class Post {
-  id = 0;
-  category = "";
-
+  id = "";
   title = "";
-  body = "";
+  user_explanation = "";
+  category = "";
 
   user = new User();
   creationDate = new Date();
@@ -19,10 +18,10 @@ class Post {
 
   static preview() {
     return Post.from({
-      id: 0,
-      category: "gatos",
+      "id": "123123123",
       title: "Cómo cuidar un gato",
-      body: "Compra una jaula grande. A los canarios les gusta volar de un lado a otro. Por lo tanto, es importante que consigas una jaula muy amplia, aunque no necesariamente alta. Si solo tienes un canario, su jaula debe medir aproximadamente 40 cm (16 pulgadas) de alto y 80 cm (30 pulgadas) de ancho. Cuanto más grande sea la jaula, mejor será para el canario. Instala perchas.Compra madera natural en la tienda de animales para colocarla a manera de perchas a lo largo de la jaula.Compra perchas de diámetros diferentes para añadir un poco de diversidad a la experiencia del canario.Debido a que a los canarios les gusta volar horizontalmente, en lugar de hacerlo hacia arriba o abajo, coloca las perchas a los lados de la jaula, una frente a la otra.",
+      user_explanation: "Compra una jaula grande. A los canarios les gusta volar de un lado a otro. Por lo tanto, es importante que consigas una jaula muy amplia, aunque no necesariamente alta. Si solo tienes un canario, su jaula debe medir aproximadamente 40 cm (16 pulgadas) de alto y 80 cm (30 pulgadas) de ancho. Cuanto más grande sea la jaula, mejor será para el canario. Instala perchas.Compra madera natural en la tienda de animales para colocarla a manera de perchas a lo largo de la jaula.Compra perchas de diámetros diferentes para añadir un poco de diversidad a la experiencia del canario.Debido a que a los canarios les gusta volar horizontalmente, en lugar de hacerlo hacia arriba o abajo, coloca las perchas a los lados de la jaula, una frente a la otra.",
+      category: "canina",
       user: User.preview(),
       creationDate: Date.now(),
       comments: [...Array(10)].map(() => Comment.preview()),
