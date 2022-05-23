4550c65a9e4d953c55cf2c3f224a049ddb7f7986
Zebra <zebrv.apps@gmail.com>
2022-05-23 19:34:03 +0200

    visual animal filter components

--- src/views/animals/AnimalList.js
+++ src/views/animals/AnimalList.js
@@ -2,11 +2,12 @@ import React, { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
 import { useEffectOnce } from "usehooks-ts";
 import { useNavigate } from "react-router-dom";
+import { Form, FloatingLabel } from "react-bootstrap";
 
 import { ListAnimal } from "../../models";
 import AnimalComponent from "./components/AnimalComponent";
 
-import { getAnimals, getStatistics } from "../../api/Api";
+import { getAnimals, getSpecies, getStatistics } from "../../api/Api";
 
 import { Pagination } from "react-bootstrap";
 
@@ -24,9 +25,19 @@ function AnimalList() {
   const [animals, setAnimals] = useState([]);
   const [totalAnimals, setTotalAnimals] = useState(0);
 
+  const [species, setSpecies] = useState([]);
+  const [filterSpecie, setFilterSpecie] = useState();
+  const [filterBreed, setFilterBreed] = useState();
+
   const animalsPerPage = 9;
 
   const loc = useLocation();
+
+  const capitalize = (str) => {
+    const lower = str.toLowerCase();
+    let firstLetter = str.charAt(0);
+    return firstLetter.toUpperCase() + lower.slice(1);
+  };
 
   const getNumPages = () => {
     if (animalsPerPage === 0) {
@@ -74,6 +85,17 @@ function AnimalList() {
       });
   }, [animalsPerPage, totalAnimals, pagina]);
 
+  useEffectOnce(() => {
+    getSpecies()
+      .then((result) => {
+        let result_species = result.data.map((specie) =>
+          capitalize(specie._id)
+        );
+        setSpecies(result_species);
+      })
+      .then((error) => { });
+  });
+
   const gotoPage = (page) => {
     navigate({
       pathname: "/animales",
@@ -87,11 +109,44 @@ function AnimalList() {
   return (
     <div className="home">
       <header className="mt-5 p-5">
-        <input
-          type={"text"}
-          placeholder="Buscar animal"
-          className="w-100 p-2"
-        />
+        <div className="row">
+          <div className="col-sm-3">
+            <FloatingLabel
+              className="mb-3"
+              controlId="floatingSelect"
+              label="Especie"
+            >
+              <Form.Select
+                required
+                aria-label="Especie"
+                value={filterSpecie}
+                onChange={(e) => setFilterSpecie(e.target.value)}
+              >
+                <option value="">
+                  Cualquier especie
+                </option>
+                {species.map((specie, i) => (
+                  <option value={specie} key={i}>
+                    {specie}
+                  </option>
+                ))}
+              </Form.Select>
+            </FloatingLabel>
+          </div>
+          <div className="col-sm-9">
+            <FloatingLabel
+              controlId="floatingTextarea2"
+              label="Buscar por raza"
+            >
+              <Form.Control
+                type="text"
+                placeholder="Buscar por raza"
+                value={filterBreed}
+                onInput={(e) => setFilterBreed(e.target.value)}
+              />
+            </FloatingLabel>
+          </div>
+        </div>
       </header>
       <div className="container">
         {table.map((row, i) => (
