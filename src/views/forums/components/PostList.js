dd4808db375f60ccc0b41ab8fd8b46e8e7147dff
Leticia-23 <760549@unizar.es>
2022-05-24 02:06:56 +0200

    format y linter

--- src/views/forums/components/PostList.js
+++ src/views/forums/components/PostList.js
@@ -2,12 +2,9 @@ import React, { useContext } from "react";
 import { UserContext } from "../../../environment";
 import PostComponent from "./PostComponent";
 import { deleteForum, deleteForumUser } from "../../../api/Api";
-import { useNavigate } from "react-router-dom";
 
 function PostList({ posts, thread = false, onDelete = () => {} }) {
   let { user: currentUser } = useContext(UserContext);
-
-  const navigate = useNavigate();
 
   const onPostDelete = (post) => {
     console.log("delete admin post with id " + post.id);
